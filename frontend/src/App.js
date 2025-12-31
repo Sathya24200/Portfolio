d7552ed23cbb46890c804f964cdec84392fdd398
Sathya24200 <sathyamoorthyr306@gmail.com>
2025-12-31 19:38:26 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Portfolio from "./pages/Portfolio";
+import DetailsPage from "./pages/DetailsPage";
+import CertificatesPage from "./pages/CertificatesPage";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+          <Route path="/details" element={<DetailsPage />} />
+          <Route path="/certificates" element={<CertificatesPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
